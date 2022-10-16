f9424c56062676e649785f01b83ba284cf9c11b8
Pratik Jagtap <pratikjagtap012@gmail.com>
2022-10-16 19:59:05 +0530

    solved

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    //getter
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,26 @@
 package com.driver.test;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 
+import com.driver.RWOnly;
+
 public class TestCases {
-    
+    private RWOnly obj = new RWOnly();
+
+    @Test
+    public final void testprintHelloWorld() {
+        obj.setName("Ritik");
+        assertEquals("Ritik", obj.getName());
+    }
+
+    @Test
+    public final void testprintHelloWorld2() {
+        obj.setName("Ritik");
+        assertNotEquals("Rithik", obj.getName());
+        // assertEquals("Method is overridden in Extendend class B", obj.getName());
+    }
 }
+
